5c545de90a8017694fc4778557c16cec999f69ad
Rick Levy <ricklevy21@gmail.com>
2020-04-20 12:48:42 -0600

    third part links added

---
+++ assets/script.js
@@ -0,0 +1,30 @@
+//FUNCTIONS
+//============================================================================================================================================================
+
+console.log(moment().format('MMMM Do YYYY, h:mm:ss a'))
+
+//GLOBAL VARIABLES
+//============================================================================================================================================================
+
+
+// EVENT LISTENERS
+//============================================================================================================================================================
+
+
+
+
+//TO DO
+//============================================================================================================================================================
+/*
+
+function that color codes each input field based on the time of day. Likely use an if statement
+function that displays the current date and time at the top. use the Moment.js library
+function that saves the user's input to local storage when the save button is clicked
+function that converts array of objects to array of strings before sending to local storage
+function that converts array of strings back to objects when retrieving from storage
+function to figure out what time it is (and store it in a variable?)
+
+array of objects to store the calendar items in
+
+
+*/
