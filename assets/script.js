2f973d4fa48e31114b45ce18bde817863039daef
Rick Levy <ricklevy21@gmail.com>
2020-04-22 14:05:52 -0600

    date displays in jumbotron

--- assets/script.js
+++ assets/script.js
@@ -3,54 +3,14 @@ $(document).ready(function() {
 
     //GLOBAL VARIABLES
 //============================================================================================================================================================
-//array of messages to appear in Jumbotron
-var messages = [
-    "YOUR ARE LATE!...jk",
-    "Cheers to a produtive day",
-    "Don't forget to wash your hands",
-    "There is always tomorrow",
-    "No time like the present",
-    "Carpe diem"
-]
-//variable for random message from messages array
-var getMessage = messages[Math.floor(Math.random()*messages.length)];
-console.log(getMessage)
 
-//CONTENT VARIABLES
-//variable for placing the current time in the Jumbotron
-var timeJumbotron = $("<p>");
-var randomMessage = $("<p>");
 
 
 //FUNCTIONS
 //============================================================================================================================================================
     //function to display the current date/time in the jumbotron
-    var displayTime = function(){
+    var displayDate = function(){
         //display the current date and time in the Jumbotron
-        timeJumbotron.text(moment().format('MMMM Do YYYY, h:mm:ss a'));
-        $("#datetimeJumbo").append(timeJumbotron);
-    }
-
-    //function to update the current time in the jumbotron every second
-    var updateTime = function(){
-        var interval = setInterval(function(){
-            displayTime();
-        },1000)
-    }
-
-    //function to display a message in the jumbotron
-    var displayMessage = function(){
-        //display a random message in the Jumbotron
-        randomMessage.text(getMessage);
-        $("#friendlyMessage").append(randomMessage);
-    }
-
-    //function to update the random meesage in the jumbotron every 3 minutes
-    var updateMessage = function(){
-        displayMessage();
-        var interval = setInterval(function(){
-        displayMessage();
-        },5000)
     }
 
 
@@ -60,8 +20,10 @@ var randomMessage = $("<p>");
 
 //PAGE CONTENT
 //============================================================================================================================================================
-updateTime();
-updateMessage();
+//display today's date in the jumbotron
+var dateJumbo = $("<p>");
+dateJumbo.text(moment().format('MMMM DD, YYYY'));
+$("#currentDay").append(dateJumbo);
 
 
 
@@ -79,11 +41,6 @@ array of objects to store the calendar items in
 function that saves the user's input to local storage when the save button is clicked
 function that converts array of objects to array of strings before sending to local storage
 function that converts array of strings back to objects when retrieving from storage
-
-
-array of messages to appear in jumbotron.
-function to select random message from array and display a new on every hour
-
 
 
 */
